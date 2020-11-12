24125b4f72255593b0a24f8266c283cffb590ed3
Zen <muhzaini30@gmail.com>
2020-11-13 01:44:57 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,20 @@ module.exports = function(eleventyConfig){
 	        return snippet + match;
 	      }
 	    }
-	  }
+	  },
+	  callbacks: {
+	      ready: function(err, bs) {
+
+	        bs.addMiddleware("*", (req, res) => {
+	          const content_404 = fs.readFileSync('_site/404.html');
+	          // Provides the 404 content without redirect.
+	          res.write(content_404);
+	          // Add 404 http status code in request header.
+	          // res.writeHead(404, { "Content-Type": "text/html" });
+	          res.writeHead(404);
+	          res.end();
+	        });
+	      }
+	    }
 	});
 }
